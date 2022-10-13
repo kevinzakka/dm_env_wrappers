df5f1566a9002b068516eaa604d0cc805ed54412
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-10-13 16:48:23 -0700

    Simpler main unit test.

--- dm_env_wrappers/test_dm_env_wrappers.py
+++ dm_env_wrappers/test_dm_env_wrappers.py
@@ -1,27 +1,15 @@
-"""Test for dm_env_wrappers."""
+"""Tests for dm_env_wrappers."""
 
-from absl.testing import absltest, parameterized
-from dm_control.suite import load
+from absl.testing import absltest
 
-from dm_env_wrappers import __all__, wrap_all
-
-_WRAPPERS = [w for w in __all__ if w.endswith("Wrapper")]
+import dm_env_wrappers
 
 
-class ImportTest(parameterized.TestCase):
-    """Test that the wrappers can be imported and used."""
+class DmEnvWrappersTest(absltest.TestCase):
+    """Test that dm_env_wrappers can be imported correctly."""
 
-    @parameterized.parameters(_WRAPPERS)
-    def test_import_and_wrap(self, wrapper_str) -> None:
-        env = load("cartpole", "balance")
-        wrapped_env = getattr(__import__("dm_env_wrappers"), wrapper_str)(env)
-        wrapped_env.reset()
-
-    def test_wrap_all(self) -> None:
-        env = load("cartpole", "balance")
-        wrapper_cls = [getattr(__import__("dm_env_wrappers"), w) for w in _WRAPPERS]
-        wrapped_env = wrap_all(env, wrapper_cls)
-        wrapped_env.reset()
+    def test_import(self) -> None:
+        self.assertTrue(hasattr(dm_env_wrappers, "EnvironmentWrapper"))
 
 
 if __name__ == "__main__":
