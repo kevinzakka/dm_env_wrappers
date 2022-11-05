dfdb6103082e3ebd6a9fe7224900b84d0ed2ce00
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-11-04 17:56:24 -0700

    Switch video saving to end of episode instead of at reset.

--- dm_env_wrappers/_src/video.py
+++ dm_env_wrappers/_src/video.py
@@ -20,15 +20,15 @@ class VideoWrapper(base.EnvironmentWrapper, abc.ABC):
     def __init__(
         self,
         environment: dm_env.Environment,
-        save_dir: str = "~/dm_env_wrappers",
-        save_every: int = 100,
+        record_dir: str = "~/dm_env_wrappers",
+        record_every: int = 100,
         frame_rate: int = 30,
     ) -> None:
         super().__init__(environment)
 
-        self._save_dir = Path(save_dir).expanduser()
-        self._save_dir.mkdir(parents=True, exist_ok=True)
-        self._save_every = save_every
+        self._record_dir = Path(record_dir).expanduser()
+        self._record_dir.mkdir(parents=True, exist_ok=True)
+        self._record_every = record_every
         self._frame_rate = frame_rate
 
         self._frames: List[np.ndarray] = []
@@ -37,11 +37,11 @@ class VideoWrapper(base.EnvironmentWrapper, abc.ABC):
     def step(self, action) -> dm_env.TimeStep:
         timestep = self.environment.step(action)
         self._append_frame(timestep.observation)
+        if timestep.last():
+            self._write_frames()
         return timestep
 
     def reset(self) -> dm_env.TimeStep:
-        if self._frames:
-            self._write_frames()
         self._counter += 1
         timestep = self.environment.reset()
         self._append_frame(timestep.observation)
@@ -50,17 +50,15 @@ class VideoWrapper(base.EnvironmentWrapper, abc.ABC):
     # Helper methods.
 
     def _append_frame(self, observation):
-        if self._counter % self._save_every == 0:
+        if self._counter % self._record_every == 0:
             self._frames.append(self._render_frame(observation))
 
     def _write_frames(self) -> None:
-        if self._counter % self._save_every == 0:
-            filename = self._save_dir / f"{self._counter:05d}.mp4"
+        if self._counter % self._record_every == 0:
+            filename = self._record_dir / f"{self._counter:05d}.mp4"
             imageio.mimsave(
                 str(filename), self._frames, fps=self._frame_rate  # type: ignore
             )
-
-        # Clear frame buffer whether or not we wrote a video.
         self._frames = []
 
     @abc.abstractmethod
