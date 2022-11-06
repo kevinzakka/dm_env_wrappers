9d7bcf0c878e16bbf666d926eb1991bb9a030bd8
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-11-05 20:23:03 -0700

    Docstrings for new wrapper.

--- dm_env_wrappers/_src/validate_spec.py
+++ dm_env_wrappers/_src/validate_spec.py
@@ -6,8 +6,13 @@ from dm_env_wrappers._src import base
 
 
 class ValidateActionSpecWrapper(base.EnvironmentWrapper):
-    """A wrapper that throws a `ValueError` if an action does not match its spec."""
+    """A wrapper that throws an exception if an action does not match its spec."""
 
     def step(self, action) -> dm_env.TimeStep:
+        """Validates the action against the action spec.
+
+        Raises:
+            ValueError: If the action does not match the action spec.
+        """
         self.action_spec().validate(action)
         return self._environment.step(action)
